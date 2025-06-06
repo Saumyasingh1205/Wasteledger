b9c8de8c5ad147b0719139e470045453bfb84973
Saumya Singh <saumyasingh5223@gmail.com>
2025-06-07 00:20:03 +0530

    your message

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
+app.use(express.static('frontend'));
 
 // Setup file upload storage
 const storage = multer.diskStorage({
